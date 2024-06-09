b51dbf9d578b2d4169e9a428a1b97b46fc69c39e
POC User <pocuser@satisfilabs.com>
2024-06-09 09:16:14 -0700

    update the name

--- selfi/Infra/Terraforms/GCP/Artficatregistry/main.tf
+++ selfi/Infra/Terraforms/GCP/Artficatregistry/main.tf
@@ -2,9 +2,9 @@
 resource "google_artifact_registry_repository" "artifactory_repository" {
   project       = var.project_id
   location      = var.region
-  repository_id = "cloud-function-artifact-regsitry"
+  repository_id = "cloud-run-artifact-regsitry"
   format        = "DOCKER"
-  description   = "cloud-function repository repository "
+  description   = "cloud-run repository repository "
 }
 
 
