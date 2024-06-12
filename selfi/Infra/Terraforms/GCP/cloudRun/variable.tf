19c13741b690285c88a453fcfb824add2b2fea45
pocuser <pocuser@satisfilabs.com>
2024-06-12 12:58:26 -0700

    Update Variable files in the master account

--- selfi/Infra/Terraforms/GCP/cloudRun/variable.tf
+++ selfi/Infra/Terraforms/GCP/cloudRun/variable.tf
@@ -10,16 +10,18 @@ variable "project_id" {
     "prod" = "satisfi-prod"
     "stage" = "satisfi-stage"
     "dev" = "sl-dev-gmni-prj"
-}+++
+}
 }
 
 variable "credentials" {
   type = map 
   description = "GCP credentials file"
   default     = { 
+    # Unix format 
     "prod" = "../satisfi-prod-fa7cb69d1b74.json"
     "core" = "../satisfi-core-fa7cb69d1b74.json"
     "stage" = "../satisfi-stage-fa7cb69d1b74.json"
+    # windows format 
     "dev"   =  "C:\\Users\\abamb\\Downloads\\sl-dev-gmni-prj-8f920a74eb81.json"
 }
 }
