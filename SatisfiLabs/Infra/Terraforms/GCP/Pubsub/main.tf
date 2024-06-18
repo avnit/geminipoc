f2f841085726fc871fa67edc05e4c70997fba2bd
POC User <pocuser@satisfilabs.com>
2024-06-17 19:58:30 -0700

    updating pubsub messages

--- SatisfiLabs/Infra/Terraforms/GCP/Pubsub/main.tf
+++ SatisfiLabs/Infra/Terraforms/GCP/Pubsub/main.tf
@@ -1,13 +1,13 @@
-resource "google_pubsub_schema" "example" {
-  name = "example"
+resource "google_pubsub_schema" "incoming-message" {
+  name = "message"
   type = "AVRO"
   definition = "{\n  \"type\" : \"record\",\n  \"name\" : \"Avro\",\n  \"fields\" : [\n    {\n      \"name\" : \"StringField\",\n      \"type\" : \"string\"\n    },\n    {\n      \"name\" : \"IntField\",\n      \"type\" : \"int\"\n    }\n  ]\n}\n"
 }
 
 resource "google_pubsub_topic" "example" {
-  name = "example-topic"
+  name = "incoming-message-topic"
 
-  depends_on = [google_pubsub_schema.example]
+  depends_on = [google_pubsub_schema.incoming-message]
   schema_settings {
     schema = "projects/my-project-name/schemas/example"
     encoding = "JSON"
