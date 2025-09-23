fc3f77b25c0148f4137199b52979be833443047e
Avnit Bambah <avnit.bambah@AlticeUSA.com>
2025-09-23 10:40:55 -0700

    updates

---
+++ cong/terraform/gke.tf
@@ -0,0 +1,24 @@
+resource "google_container_cluster" "primary" {
+  name     = "primary-gke-cluster"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.main.name
+  subnetwork = google_compute_subnetwork.main.name
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
